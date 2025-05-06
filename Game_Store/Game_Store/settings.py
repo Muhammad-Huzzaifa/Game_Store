9ab49cc78109a1324b9b3fc15e6d99b938e16846
Muhammad-Huzzaifa <huzzaaifa83@gmail.com>
2025-05-06 16:21:46 +0500

    setted templates structure

--- Game_Store/Game_Store/settings.py
+++ Game_Store/Game_Store/settings.py
@@ -60,7 +60,10 @@ ROOT_URLCONF = 'Game_Store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'store', 'templates'),
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'store', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
