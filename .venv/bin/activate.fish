9900a6b90823f1fda62a04b42aeb22692197fa6d
Maarij-Aqeel <maarijaqeel3200@gmail.com>
2025-05-09 10:33:04 +0500

    Fixed merge issue in requirements

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/maarij-aqeel/Documents/Other/Websites/Game_Store_DB_Project/.venv
+set -gx VIRTUAL_ENV /home/maarij-aqeel/Documents/Other/Websites/Game_Store/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
